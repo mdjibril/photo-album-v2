ecd85a343b0c061c21e5f78a9eb48461d555fd3e
mdjibril <muhammadjibrildauda@gmail.com>
2023-07-03 11:09:13 +0100

    main album page

---
+++ album.php
@@ -0,0 +1,113 @@
+<?php
+// Start the session
+session_start();
+require 'connection.php';
+// Check if the user is logged in
+if (!isset($_SESSION["email"])) {
+	header("Location: ../index.php");
+	exit();
+}
+
+// Connect to the database
+$servername = "localhost";
+$username = "root";
+$password_db = "";
+$dbname = "album";
+
+$conn = mysqli_connect($servername, $username, $password_db, $dbname);
+
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+// Get the user's record from the database
+// $email = $_SESSION["email"];
+$i = 0;
+$sql = "SELECT * FROM `user` WHERE `image` IS NOT NULL";
+$result = mysqli_query($conn, $sql);
+
+// if (mysqli_num_rows($result) > 0) {
+// 	// User record found, display the dashboard
+// 	$row = mysqli_fetch_assoc($result);
+// 	$fullname = $row["fullname"];
+// 	$age = $row["age"];
+// 	$image = $row["image"];
+// 	$regno = $row["regno"];
+// 	$phone = $row["phone"];
+// 	$nickname = $row["nickname"];
+// 	$department = $row["department"];
+// 	$address = $row["address"];
+// 	$status = $row["status"];
+// 	// echo $status;
+// } else {
+// 	// User record not found, show error message
+// 	echo "<p>There was an error retrieving your record. Please update your record.</p>";
+// }
+
+// mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ALBUM PAGE</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<style>
+		header {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+			height: 50px;
+			margin-bottom: 10px;
+			color: #f9f9f9;
+			background-color: #4caf50;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>CLASS OF 2015</h1>
+	</header>
+	<main class="flexray">
+	<?php while ($row = mysqli_fetch_array($result)){ 
+		$i++;
+		$fullname = $row["fullname"];
+		$age = $row["age"];
+		$image = $row["image"];
+		$regno = $row["regno"];
+		$phone = $row["phone"];
+		$email = $row["email"];
+		$nickname = $row["nickname"];
+		$department = $row["department"];
+		$address = $row["address"];
+		$status = $row["status"];
+
+		?>
+		<div class="card">
+		  <div class="card-side front">
+		    <!-- <div>Front Side</div> -->
+		    <img src="uploads/<?php echo $image; ?>" >
+		  </div>
+		  <div class="card-side back">
+		    <p>Name: <?php echo $fullname ?></p>
+			<br>
+		    <div>Age: <?php echo $age ?></div>
+			<br>
+		    <div>Regno: <?php echo $regno ?></div>
+			<br>
+		    <div>Phone: <?php echo $phone ?></div>
+			<br>
+			<div>Email: <?php echo $email ?></div>
+			<br>
+		    <div>Nick: <?php echo $nickname ?></div>
+			<br>
+		    <div>Dept. : <?php echo $department ?></div>
+		  </div>
+		</div>
+	<?php } ?>
+	</main>
+</body>
+</html>
