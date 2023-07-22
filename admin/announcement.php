5c732f17e7e0bb8b44a39872ff60a47bd2cd5630
mdjibril <muhammadjibrildauda@gmail.com>
2023-07-22 23:20:24 +0100

    deleted the modal

--- admin/announcement.php
+++ admin/announcement.php
@@ -22,102 +22,6 @@ require '../connection.php';
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     
-    <style>
-        /* The Modal (background) */
-        .modal {
-            display: none;
-            /* Hidden by default */
-            position: fixed;
-            /* Stay in place */
-            z-index: 1;
-            /* Sit on top */
-            padding-top: 100px;
-            /* Location of the box */
-            left: 0;
-            top: 0;
-            width: 100%;
-            /* Full width */
-            height: 100%;
-            /* Full height */
-            overflow: auto;
-            /* Enable scroll if needed */
-            background-color: rgb(0, 0, 0);
-            /* Fallback color */
-            background-color: rgba(0, 0, 0, 0.4);
-            /* Black w/ opacity */
-        }
-
-        /* Modal Content */
-        .modal-content {
-            position: relative;
-            background-color: #fefefe;
-            margin: auto;
-            padding: 0;
-            border: 1px solid #888;
-            width: 80%;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-            -webkit-animation-name: animatetop;
-            -webkit-animation-duration: 0.4s;
-            animation-name: animatetop;
-            animation-duration: 0.4s
-        }
-
-        /* Add Animation */
-        @-webkit-keyframes animatetop {
-            from {
-                top: -300px;
-                opacity: 0
-            }
-
-            to {
-                top: 0;
-                opacity: 1
-            }
-        }
-
-        @keyframes animatetop {
-            from {
-                top: -300px;
-                opacity: 0
-            }
-
-            to {
-                top: 0;
-                opacity: 1
-            }
-        }
-
-        /* The Close Button */
-        .close {
-            color: white;
-            float: right;
-            font-size: 28px;
-            font-weight: bold;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: #000;
-            text-decoration: none;
-            cursor: pointer;
-        }
-
-        .modal-header {
-            padding: 2px 16px;
-            background-color: #5cb85c;
-            color: white;
-        }
-
-        .modal-body {
-            padding: 2px 16px;
-        }
-
-        .modal-footer {
-            padding: 2px 16px;
-            background-color: #5cb85c;
-            color: white;
-        }
-    </style>
 </head>
 
 <body id="page-top">
@@ -285,7 +189,7 @@ require '../connection.php';
                         </div>
                         <form class="user" method="POST" action="script/announcementscript.php">
                             <div class="form-group">
-                                <textarea name="message" id="" cols="30" rows="5" class="form-control">
+                                <textarea name="message" id="" cols="30" rows="5" class="form-control" value="Hello there">
                                 </textarea>
                             </div>
                             <div class="form-group">
@@ -357,7 +261,7 @@ require '../connection.php';
                                                 <td>
                                                     <?php
                                                     echo "
-                                                    <a href='#' id='myBtn'>Edit ||</a>
+                                                    <a href='editmessage.php?ids=$id'>Edit ||</a>
                                                     <a href='script/deletemessage.php?ids=$id'> Delete</a>
                                                     ";
                                                     ?>
@@ -415,39 +319,6 @@ require '../connection.php';
         </div>
     </div>
 
-    <!-- modal for edit -->
-    <div id="myModal" class="modal">
-        <!-- Modal content -->
-        <div class="modal-content">
-            <div class="modal-header">
-                <span class="close">&times;</span>
-                <h2 style="text-align:center">Update Record</h2>
-            </div>
-            <div class="modal-body">
-                <form class="user" method="POST" action="script/announcementscript.php">
-                    <div class="form-group">
-                        <textarea name="message" id="" cols="30" rows="5" class="form-control">
-                                </textarea>
-                    </div>
-                    <div class="form-group">
-                        <input type="date" class="form-control" name="date">
-                    </div>
-                    <div class="form-group">
-                        <select name="status" id="" class="form-control">
-                            <option value="">Announcement - Status</option>
-                            <option value="0">0 - Hidden</option>
-                            <option value="1">1 - Visible</option>
-                        </select>
-                    </div>
-                    <input class="btn btn-primary btn-user btn-block" type="submit" name="submit" value="Send">
-                </form>
-            </div>
-            <div class="modal-footer">
-                <h3>&nbsp;</h3>
-            </div>
-        </div>
-    </div>
-
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -464,34 +335,6 @@ require '../connection.php';
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
-
-    <script>
-        // Get the modal
-        var modal = document.getElementById('myModal');
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    </script>
 </body>
 
 </html>
